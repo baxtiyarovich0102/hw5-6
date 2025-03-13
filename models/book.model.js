9e0fc0737aaeadff080b63ffa526c5da7864bfc0
Shahriyor <baxtiyarovich0102@gmail.com>
2025-03-13 16:17:04 +0500

    2ta nodel tugatildi

---
+++ models/book.model.js
@@ -0,0 +1,15 @@
+const {default : mongoose} = require("mongoose")
+const { type } = require("os")
+
+const bookSchema = new mongoose.Schema({
+    title : {type: String, required : true, unique: true},
+    author : {type: String, required: true},
+    genre : {type: String, required: true},
+    description : {type: String},
+    status : {type: String, enum: ["Mavjud", "Band"], default: "Mavjud"},
+    image : {type: String}
+})
+
+let book = mongoose.model("Book", bookSchema)
+
+module.exports = book
---
+++ models/user.model.js
@@ -0,0 +1,10 @@
+const {default : mongoose} = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name : {type: String, required : true, unique: true},
+    password : {type: String, required : true}
+})
+
+let user = mongoose.model("User", userSchema)
+
+module.exports = user
